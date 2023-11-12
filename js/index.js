0d0723e495673cdc9f23bad055725a557642de84
RuslanSamburov <samburov.com@gmail.com>
2023-11-12 14:37:20 +0300

    root.css: word-break: break-word

--- js/index.js
+++ js/index.js
@@ -4,3 +4,7 @@ let app = new App(App.createApp('app'));
 app.header.center = '<span class="bold">Профиль<span>';
 
 root.add(app.app);
+
+let book = new Book(Book.createBook(['sdfsdfsdfsdfssdffdsfsd', '312', '321'], ['sdfsdfasdfsadfasdfasdfasdfasdfasfdasdfasdfasdfafsdsafasdfsaasfd', 'sdfsdfsdfsfd', 'sdfsdfsdfsdf']));
+
+app.body.append(book.book)
